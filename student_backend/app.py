b055caa4d3e08eb0b62c1ead711cfd3ea521052a
prxshant07 <prashantrajput7116@gmail.com>
2026-03-21 02:48:11 +0530

    production

--- student_backend/app.py
+++ student_backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
@@ -352,4 +353,5 @@ def generate_insights(study_hours, attendance, participation, predictions):
     return insights
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
