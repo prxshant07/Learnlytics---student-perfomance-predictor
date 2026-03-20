f98c91e9f9848e1c07991a69e77df52202895252
prxshant07 <prashantrajput7116@gmail.com>
2026-03-21 01:54:46 +0530

    hosting

--- student_backend/app.py
+++ student_backend/app.py
@@ -352,4 +352,4 @@ def generate_insights(study_hours, attendance, participation, predictions):
     return insights
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
